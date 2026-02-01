0ae89f9fc1999b2c1c0e708f072681438ab456cc
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-01 16:52:13 +0100

    Initialize main.py to load data and launch GUI

---
+++ main.py
@@ -0,0 +1,7 @@
+from data.dataset import load_data
+from model.spam_model import train_model
+from gui.app import launch_gui
+
+df = load_data()
+model, vectorizer = train_model(df)
+launch_gui(model, vectorizer)
