9a5a491836db01f96e025eea49efee80a6775378
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-02 15:02:13 +0100

    Loading data then training a model

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from data.dataset import load_data
 from model.spam_model import train_model
 from gui.app import launch_gui
-
 df = load_data()
 model, vectorizer = train_model(df)
 launch_gui(model, vectorizer)
