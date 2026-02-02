3c2f89eb3dd0c669b512d092b7955e2834a0d0ce
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-02 14:59:06 +0100

    Implement spam model training function

--- model/spam_model.py
+++ model/spam_model.py
@@ -5,15 +5,10 @@ from sklearn.svm import SVC
 def train_model(df):
     X = df['Message']
     y = df['Label']
-
     X_train, _, y_train, _ = train_test_split(
-        X, y, test_size=0.2, random_state=0
-    )
-
+        X, y, test_size=0.2, random_state=0)
     vectorizer = TfidfVectorizer()
     X_train_tfidf = vectorizer.fit_transform(X_train)
-
     model = SVC(kernel='rbf', gamma=0.2, C=1.0)
     model.fit(X_train_tfidf, y_train)
-
     return model, vectorizer
